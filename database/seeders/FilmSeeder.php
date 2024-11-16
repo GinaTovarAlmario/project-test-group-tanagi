b9c0d38e4b908516151d658d10fafe5ff6150bf7
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 16:06:59 +0100

    added kafer for data in seeder

--- database/seeders/FilmSeeder.php
+++ database/seeders/FilmSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Film;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class FilmSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i = 0; $i < 100 ; $i++){
+            $newFilm = new Film();
+            $newFilm->title = $faker->realTextBetween(10,60);
+            $newFilm->director = $faker->name();
+            $newFilm->date_of_release = $faker->date();
+            $newFilm->description = $faker->realTextBetween(300,800);
+            $newFilm->save();
+        }
     }
 }
