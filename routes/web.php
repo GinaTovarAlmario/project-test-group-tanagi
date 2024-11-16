3be734c1f0a4527bda3a711af92f88a8b7eeb5ad
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 15:46:22 +0100

    added show route

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,9 @@ Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function(){
     Route::get('/films/create',[AdminFilmController::class,'create'])->name('films.create');
     Route::get('/films/{id}',[AdminFilmController::class,'destroy'])->name('films.delete');
     Route::put('/films/{id}',[AdminFilmController::class,'update'])->name('films.update');
-    
+    Route::put('/films/{id}',[AdminFilmController::class,'show'])->name('films.show');
+
+
 
 
 
