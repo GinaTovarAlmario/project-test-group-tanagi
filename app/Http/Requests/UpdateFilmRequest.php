addceda26a122929041fbcb0d793fd796fc0b090
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 22:07:51 +0100

    added customized error

--- app/Http/Requests/UpdateFilmRequest.php
+++ app/Http/Requests/UpdateFilmRequest.php
@@ -32,4 +32,28 @@ class UpdateFilmRequest extends FormRequest
             'description'=>'required|string|min:2|max:1000',
         ];
     }
+    public function messages(){
+        return[
+            'title.required'=>'Il titolo è obbligatorio',
+            'title.string'=>'Il titolo deve essere un testo!',
+            'title.min' => 'Il titolo deve essere lungo minimo 3 caratteri!',
+            'title.max' => 'Troppo lungo!Il testo deve essere più breve!',
+
+            'date_of_release.required'=>'La data di rilascio è obbligatoria',
+            'date_of_release.data'=>'La data deve essere nel formato YYYY-MM-DD',
+
+            'director.required'=>'Nome direttore è obbligatorio',
+            'director.string'=>'Il nome direttore deve essere un testo!',
+            'director.min' => 'Il nome deve essere lungo minimo 3 caratteri!',
+            'director.max' => 'Troppo lungo!Il nome deve essere più breve!',
+
+
+
+            'description.required'=>'La descrizione è obbligatoria',
+            'description.string'=>'La descrizione deve essere un testo!',
+            'description.min'=>'La descrizione è troppo breve!',
+            'description.max'=>'La descrizione è troppa lunga!',
+
+        ];
+    }
 }
