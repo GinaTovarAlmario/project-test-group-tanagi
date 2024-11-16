0ecd484146d3e7e80ce2af444d294b0d79edc143
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 19:02:00 +0100

    added method index to api film controller

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Film;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
 {
-    //
+    public function index()
+    {
+        $films = Film::all();
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $films,
+            ]
+        );
+    }
 }
