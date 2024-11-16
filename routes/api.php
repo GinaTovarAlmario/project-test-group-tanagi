712a6eadb63e2b9db0d583deafd47aff324c7fb0
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 18:58:54 +0100

    added route for api index

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\FilmController;
+use Faker\Guesser\Name;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/films',[FilmController::class,'index'])->name('api.films.index');
